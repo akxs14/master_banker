cfc7fa8064526b48ca21611b9e68500715352655
angelos <angelos.kapsimanis@gmail.com>
2014-08-10 01:36:20 +0200

    Simplified code, fetching currency symbol correctly

--- apps/master_banker/src/mnesia_manager.erl
+++ apps/master_banker/src/mnesia_manager.erl
@@ -3,67 +3,64 @@
 
 -include_lib("stdlib/include/qlc.hrl").
 
+-include("campaign.hrl").
+-include("currency.hrl").
 -include("node_campaign_budget.hrl").
 -include("banker_campaign_budget.hrl").
--include("currency.hrl").
 
 %%-----------------------------------------------------------------------------
 %% API Function Exports
 %%-----------------------------------------------------------------------------
 
--export([create_mnesia_schema/0]).
+-export([
+  create_mnesia_schema/1,
+  save_currency/1,
+  find_currency_symbol/1
+  ]).
 
 %%-----------------------------------------------------------------------------
 %% API Function Definitions
 %%-----------------------------------------------------------------------------
 
-create_mnesia_schema() ->
-  CreateCampaignBudgets = fun() ->
-    create_campaign_budgets()
-  end,
-  create_table(CreateCampaignBudgets),
-  CreateNodeBudgets = fun() ->
-    create_node_budgets()
-  end,
-  create_table(CreateNodeBudgets),
-  CreateCurrencies = fun() ->
-    create_node_budgets()
-  end,
-  create_table(CreateCurrencies).
+create_mnesia_schema(Nodes) ->
+  create_campaign_budgets(Nodes),
+  create_node_budgets(Nodes),
+  create_currencies(Nodes).
+
+save_currency(Currency) ->
+  mnesia:activity(transaction, fun() ->
+    mnesia:write(Currency)
+  end).
+
+find_currency_symbol(Campaign) ->
+  Results = mnesia:activity(transaction, fun() ->
+    mnesia:read({currency, Campaign#campaign.currency_id})
+  end),
+  case hd(Results) of
+    {currency, _, _, Symbol, _} ->
+      Symbol;
+    _ ->
+      <<"Unknown">>
+  end.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
-
-create_table(Create) ->
-  case catch Create() of
-    {'EXIT',_} ->
-      Create();
-    _ -> 
-      ok
-  end.
-
-create_campaign_budgets() ->
-  io:format("create_campaign_budgets()~n~n"),
+create_campaign_budgets(Nodes) ->
   mnesia:create_table(banker_campaign_budgets, 
     [{attributes, record_info(fields, banker_campaign_budget)},
     {index, [#banker_campaign_budget.campaign_id]},
-    {ram_copies, [node()]}]
-  ).
+    {ram_copies, Nodes}]).
 
-create_node_budgets() ->
-  io:format("create_node_budgets()~n~n"),
+create_node_budgets(Nodes) ->
   mnesia:create_table(node_campaign_budget, 
     [{attributes, record_info(fields, node_campaign_budget)},
     {index, [#node_campaign_budget.node_id]},
-    {ram_copies, [node()]}]
-  ).
+    {ram_copies, Nodes}]).
 
-create_currencies() ->
-  io:format("create_currencies()~n~n"),
-  mnesia:create_table(currencies,
+create_currencies(Nodes) ->
+  mnesia:create_table(currency,
     [{attributes, record_info(fields, currency)},
     {index, [#currency.id]},
-    {ram_copies, [node()]}]
-  ).
+    {ram_copies, Nodes}]).
